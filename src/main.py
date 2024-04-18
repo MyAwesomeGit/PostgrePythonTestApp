aaaf4044c8a5ac8baacab27fc7ccd31a106d7298
Vasily Shuklin <b88bb@yandex.ru>
2024-04-18 20:00:20 +0500

    Refactor App

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import psycopg2
 import traceback
 
-from PostgrePythonTest.models.config import host, user, password, port, db_name
+from models.config import host, user, password, db_name, port
 
 try:
     connection = psycopg2.connect(
